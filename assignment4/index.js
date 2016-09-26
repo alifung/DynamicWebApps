1b84a027520e6fe307ebc33a0ea127b06bc3f863
alifung <af2620@nyu.edu>
2016-09-26 14:55:07 -0400

    replace

--- assignment4/index.js
+++ assignment4/index.js
@@ -1,42 +1,29 @@
 var express = require('express');
 var app = express();
-var Request = require('request');
-var url = 'https://api.spotify.com/v1/search';
 
 
-//routes!
+
+/*attempt at trying to reach api through routes... not so successful!
 app.get(function(req, res) {
 	//request playlist from database
-	req.get('url' + 'data.q' + 'playlist');
+	req.get('url' + 'data.q' + 'playlist'); //request URL depending on search query input?
 	app.get('api.spotify.com/v1/search/:type?');
 	req.query.type.search;
-	req.protocol('https');
-	res.send('Here are your playlist results');
+	req.protocol('https'); //is this related to the var http://
+	res.send('Here are your playlist results'); //ideally what shows up after it's successful?
 });
 
-//attempt at trying to retrieve server through 
-//main page route
-/*	var dataForThePage= {
-		message: currentWord,
-	};
-})
-
-//json serving route
-app.get("search", function(req, res){
-	var requestURL = 'http://api.spotify.com/v1/search?type=playlist&/' + searchWord;
-	Request(requestURL, function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-			var theData = JSON.parse(body);
-			res.json(theData);
-		}
-	});
-});
-
-/*app.use(function(req, res) {
-	response.writeHead(200, {"Content-Type": "text/plain" })
-	response.end("Hello World\n");
-});
 */
+app.get('/',function(req,res){
+    res.send('<h1>yo peepz<h1>');
+});
+
+
+//last resort before 404! 
+app.use(express.static(__dirname + '/public'));
+
+
+//404 error! BUM BUM BUMMMM
 app.get("*", function(req, res) {
 	res.status(404);
 	res.send('OH NO! 404 error!');
